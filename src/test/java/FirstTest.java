d200735201c9139afd290f5ba794911b24784ff0
Chakradhar Kaitepalli <chakradhar.kaitepalli@scdhhs.gov>
2017-12-01 14:28:24 -0500

    Adding firefox support and some more unit tests.

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -27,9 +30,21 @@ public class FirstTest extends TestBase {
     @Test
     public void GOOGLE3() throws Exception {
         System.out.println("Google3 Test Started! " + Thread.currentThread().getId());
+        System.out.println("Google Test Started! " + Thread.currentThread().getId());
         getDriver().navigate().to("http://www.google.com");
-        System.out.println("Google3 Test's Page title is: " + getDriver().getTitle() +" "+ Thread.currentThread().getId());
+        getDriver().manage().window().maximize() ;
+        System.out.println("Google Test's Page title is: " + getDriver().getTitle() +" "+ Thread.currentThread().getId());
         Assert.assertEquals(getDriver().getTitle(), "Google");
+        WebElement tb = getDriver().findElement(By.id("lst-ib"));
+        tb.sendKeys("Carolina");
+        tb.sendKeys(Keys.ENTER);
+        //getDriver().findElement(By.name("btnK")).click();
+        getDriver().findElement(By.linkText("Carolina - Wikipedia")).click();
+        Thread.sleep(2000);
+        Assert.assertEquals(getDriver().getTitle(), "Carolina - Wikipedia");
+        System.out.println("Google Test Ended! " + Thread.currentThread().getId());
+        Thread.sleep(2000);
         System.out.println("Google3 Test Ended! " + Thread.currentThread().getId());
     }
+    
 }
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,9 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -8,20 +11,20 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class TestBase {
 
     //Declare ThreadLocal Driver (ThreadLocalMap) for ThreadSafe Tests
     protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
-
+	
+//    static {
+//		  System.setProperty("webdriver.gecko.driver", "/Users/Developer/Software/SeleniumDrivers/geckodriver");	
+//	}
+	
     @BeforeMethod
     @Parameters(value={"browser"})
     public void setupTest (String browser) throws MalformedURLException {
         //Set DesiredCapabilities
         DesiredCapabilities capabilities = new DesiredCapabilities();
-
         //Firefox Profile Settings
         /*if (browser=="firefox") {
             FirefoxProfile profile = new FirefoxProfile();
@@ -36,10 +39,16 @@ public class TestBase {
 
         //Set BrowserName
         capabilities.setCapability("browserName", browser);
-
+        
         //Set Browser to ThreadLocalMap
-//        driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities));
-        driver.set(new RemoteWebDriver(new URL("http://192.168.99.100:31582/wd/hub"), capabilities));
+        if (browser !=null && !"".equals(browser)) {
+//          driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities));
+            driver.set(new RemoteWebDriver(new URL("http://192.168.99.100:31582/wd/hub"), capabilities));
+        } else {
+        		FirefoxDriver fxDriver = new FirefoxDriver();
+            fxDriver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+        	    driver.set(fxDriver);
+        }
     }
 
     public WebDriver getDriver() {
@@ -52,7 +61,8 @@ public class TestBase {
         getDriver().quit();
     }
 
-    @AfterClass void terminate () {
+    @AfterClass 
+    void terminate () {
         //Remove the ThreadLocalMap element
         driver.remove();
     }
